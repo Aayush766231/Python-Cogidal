5c3efb8044fff3aeeb279d9e0bda5f080c2c817d
Aayush766231 <thisisaayush2.0@gmail.com>
2025-06-03 11:29:44 -0500

    initial commit

---
+++ Lesson 2 Activity 2/variable.py
@@ -0,0 +1,13 @@
+a = 5
+b = "Aayush"
+print(a, b)
+
+country = input("Enter your country name: ")
+print("My country name is", country)
+
+hobby = input("What is your hobby? ")
+print(f"{hobby} sounds fun")
+#f = formatted string (before "")
+
+mood = input("How are you feeling today? ")
+print(f"I'm happy that you're feeling {mood}")
