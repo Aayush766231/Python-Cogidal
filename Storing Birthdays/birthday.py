f045529fc9105f517f2ca9e6cef6169562379082
Aayush766231 <thisisaayush2.0@gmail.com>
2025-06-03 11:54:36 -0500

    initial commit

---
+++ Storing Birthdays/birthday.py
@@ -0,0 +1,12 @@
+Jan_Birthday = "January 1, 2025"
+Mar_Birthday = "March 14, 2025"
+April_Birthday = "April 5, 2025"
+Jun_Birthday = "June 28, 2025"
+Nov_Birthday = "November 19, 2025"
+
+print("Here are five birthdays: ")
+print(Jan_Birthday)
+print(Mar_Birthday)
+print(April_Birthday)
+print(Jun_Birthday)
+print(Nov_Birthday)
