2eb4584f60cd73d0a7e1fcbc1d675e129933fec9
Aayush766231 <thisisaayush2.0@gmail.com>
2025-06-17 11:35:09 -0500

    initial commit

---
+++ Lesson 4 Activity 3/percentage.py
@@ -0,0 +1,8 @@
+math = int(input("What was your marks on math? "))
+science = int(input("What was your marks on science? "))
+english = int(input("What was your marks on english? "))
+hindi = int(input("What was your marks on Hindi? "))
+
+totalPercent = ((math + science + english + hindi)/80)*100
+
+print(totalPercent, "%")
