c0531e3a5ae970ee6e55d5429a5e183f373d1b91
Aayush766231 <thisisaayush2.0@gmail.com>
2025-07-01 11:33:31 -0500

    initial commit

---
+++ Lesson 8/mean.py
@@ -0,0 +1,13 @@
+mean1 = 38
+wrongNum = 36
+correctNum = 56
+totalNums = 40
+
+sum1 = mean1 * totalNums
+print("mean * total = ", sum1)
+
+sum2 = sum1 - (wrongNum - correctNum)
+print("num1 - (wrongNum - correctNum) = ", sum2)
+
+mean2 = sum2 / totalNums
+print("The correct mean is", mean2)
