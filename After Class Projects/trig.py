a17ad5554e061c29a1bea41e6eee1960fbd4f21b
Aayush766231 <thisisaayush2.0@gmail.com>
2025-08-17 16:26:05 -0500

    initial commit

---
+++ After Class Projects/trig.py
@@ -0,0 +1,7 @@
+import math
+
+degree = int(input("Enter a number in degrees: "))
+
+print(f"Sine of {degree} is equal to {math.sin(math.radians(degree))}")
+print(f"Cosine of {degree} is equal to {math.cos(math.radians(degree))}")
+print(f"Tangent of {degree} is equal to {math.tan(math.radians(degree))}")
