c554e431e0d0041543748629def91524eba3cd73
Aayush766231 <thisisaayush2.0@gmail.com>
2025-08-24 10:19:20 -0500

    initual commit

---
+++ Lesson 21/listPractice.py
@@ -0,0 +1,16 @@
+list1 = []
+print(list1)
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+bigList = [100, 200, 300, 400, 500]
+bigList.reverse()
+print(bigList)
+
+aList = ["a", "b", "c", "d", "e"]
+print(aList)
+print(aList[::-1])
+
+tripleList = [1, 2, 3] * 3
+print(tripleList)
