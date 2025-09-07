7decaa4766cccd1321d1246073d5a49169a63668
Aayush766231 <thisisaayush2.0@gmail.com>
2025-09-07 10:37:40 -0500

    initial commit

---
+++ Lesson 25/exit.py
@@ -0,0 +1,7 @@
+for i in range(1, 10):
+    # once we reach 5, we terminate the code since we do not want to run anything more
+    if i == 5:
+        print(exit)
+        exit()
+    # as long as i is not 5, we will keep printing
+    print(i)
