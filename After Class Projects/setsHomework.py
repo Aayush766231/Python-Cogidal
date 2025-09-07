ea5e49c24cf4cea038afa344dabf8d7773f6ea1b
Aayush766231 <thisisaayush2.0@gmail.com>
2025-09-07 10:59:31 -0500

    initial commit

---
+++ After Class Projects/setsHomework.py
@@ -0,0 +1,9 @@
+#SYMMETRIC DIFFERENCE IN STRINGS
+greetings = {"Hello", "Welcome", "Hi"}
+salutations = {"Hello", "How have you been", "BuhBye"}
+print(greetings.symmetric_difference(salutations))
+
+#SYMMETRIC DIFFERENCE IN NUMBERS
+initialNums = {9, 2, 5, 7}
+secondNums = [8, 3, 2, 6]
+print(initialNums.symmetric_difference(secondNums))
