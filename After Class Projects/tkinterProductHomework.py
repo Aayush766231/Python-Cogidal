7121a5ee83d80d9027f1b36f54af980dec4e490e
Aayush766231 <thisisaayush2.0@gmail.com>
2025-11-08 07:51:09 -0600

    initial commit

---
+++ After Class Projects/tkinterProductHomework.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+window = Tk()
+window.title('Multiplication via tKinter')
+window.geometry('400x300')
+window.configure(bg = 'darkgray')
+
+lbl = Label(text= 'Hello!', fg= 'white', bg= 'black', height=1, width=300)
+num1Lbl = Label(text= 'Enter first number', fg = 'white', bg = 'gray', height=1, width=300)
+numEntry1 = Entry()
+numEntry1.focus_set()
+
+num2Lbl = Label(text= 'Enter second number here', fg = 'white', bg = 'gray', height=1, width=300)
+numEntry2 = Entry()
+
+def display():
+    try:
+        num1 = numEntry1.get()
+        num2 = numEntry2.get()
+        global product
+        product = float(num1) * float(num2)
+        textBox.delete(1.0, END)
+        textBox.insert(END, f'{num1}')
+        textBox.insert(END, f' times {num2}')
+        textBox.insert(END, f' is {product}')
+    except ValueError:
+        textBox.delete(1.0, END)
+        textBox.insert(END, 'Please enter valid numbers')
+
+textBox = Text(height=3)
+button = Button(text= 'Click to Multiply!', height=1, fg = 'white', bg = 'black', command= display)
+
+numEntry1.bind('<Return>', lambda numEntry: numEntry2.focus_set())
+
+lbl.pack()
+num1Lbl.pack()
+numEntry1.pack()
+num2Lbl.pack()
+numEntry2.pack()
+button.pack()
+textBox.pack()
+
+window.mainloop()
